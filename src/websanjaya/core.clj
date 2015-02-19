1342b820b3be10c675d58607cdd4ce997bf8fe98
Rohit Saxena <aerohit.saxena@gmail.com>
2015-02-19 12:08:15 +0100

    A toy example of how different requests could be handled differently by ring handler.

--- src/websanjaya/core.clj
+++ src/websanjaya/core.clj
@@ -1,10 +1,18 @@
 (ns websanjaya.core
   (:require [ring.adapter.jetty :as rjetty]))
 
+(defn response-for [request]
+  (let [uri (:uri request)]
+    (cond
+      (= uri "/")        "This is the home page"
+      (= uri "/hello")   "Hello from Ring!"
+      (= uri "/goodbye") "Goodbye from Ring!"
+      :else              "I don't know you")))
+
 (defn app-handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello from Ring"})
+   :body (response-for request)})
 
 (defn -main
   [& args]
