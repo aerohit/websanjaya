b05794790b5b0f847df2853eaf16adab20d186b4
Rohit Saxena <aerohit.saxena@gmail.com>
2015-02-19 11:30:35 +0100

    Updated project description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject websanjaya "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Web crawler for HN, Reddit etc."
+  :url "https://github.com/aerohit/websanjaya"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
